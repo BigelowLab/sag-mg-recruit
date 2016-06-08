17701eaee000bbb43dba79d268e9e7f559c0a133
juliambrosman <julia@bigelow.org>
2016-06-08 09:52:28 -0400

    Saginaw comparison against all 15 SAGs

--- sag_mg_recruit/sample.py
+++ sag_mg_recruit/sample.py
@@ -14,10 +14,6 @@ def cli(obj):
     pass
 
 
-@cli.command('sample', short_help='sample reads using seqtk')
-@click.option('--n', type=click.INT, help='number of reads to subsample')
-@click.option('--fastq', help='fastq to subsample')
-@click.option('--outfile', help="name of output file")
 def run_seqtk_sample(fastq, outfile, n, seed=37):
     """Subsample incoming paired-end fastqs to `n` reads (serially).
 
@@ -41,36 +37,13 @@ def run_seqtk_sample(fastq, outfile, n, seed=37):
     return outfile
 
 
-def run_kmernorm(fastq, out_file, options, cores=1):
-    """
-    perform digital normalization using kmernorm. unzip input file if gzipped
-    and gzip input file after kmernorm.
-
-    parameters
-        fastq : file path; gzipped input will be decompressed and recompressed
-        out_file : file path; file ending with '.gz' will be compressed
-        options : kmernorm options
-        cores : cpu cores to utilize
-
-    returns
-        normalized file path and surviving read pairs : tuple (string, int)
-    """
-    if file_exists(out_file):
-        return out_file
-
-    gzip_result = True if out_file.endswith('.gz') else False
-    out_file = out_file.rsplit('.gz', 1)[0]
-    if fastq.endswith(".gz"):
-        fastq = gunzip_file(fastq)
-    with file_transaction(out_file) as tx:
-        run("kmernorm %s %s > %s" % (options, fastq, tx),
-            description="Normalizing with kmernorm")
-    if gzip_result:
-        out_file = pigz_file(out_file, cores)
-    # compress input file regardless
-    fastq = pigz_file(fastq, cores)
-    return out_file
-
+@cli.command('sample', short_help='sample reads using seqtk')
+@click.option('--n', type=click.INT, help='number of reads to subsample')
+@click.option('--fastq', help='fastq to subsample')
+@click.option('--outfile', help="name of output file")
+def sample(fastq, outfile, n):
+    out = run_seqtk_sample(fastq, outfile, n)
+    return out
 
 if __name__ == '__main__':
     cli()
