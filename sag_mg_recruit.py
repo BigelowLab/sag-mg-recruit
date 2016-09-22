54792b45dc1c1ab3653ce0b1e7993c855c8a023f
juliambrosman <julia@bigelow.org>
2016-09-22 16:22:28 -0400

    bump version

--- sag_mg_recruit.py
+++ sag_mg_recruit.py
@@ -27,7 +27,7 @@ from distutils.spawn import find_executable
 from scgc.utils import file_transaction, safe_makedir, run, tmp_dir, pigz_file
 
 
-__version_info__ = (0, 0, 0)
+__version_info__ = (0, 0, 1)
 __version__ = '.'.join(map(str, __version_info__))
 REQUIRES = ["bedtools", "samtools", "checkm", "bwa", "gzip", "gunzip"]
 
